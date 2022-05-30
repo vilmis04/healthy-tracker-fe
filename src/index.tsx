3a19d7ce6e111f6a6bcd6637f776460a673c0eaa
Vilmantas Sudaris <vilmantas.sudaris@gmail.com>
2022-05-30 20:27:25 +0300

    Router config added

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './Components/Login/Login';
+import Dashboard from './Components/Dashboard/Dashboard';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+    <Routes>
+        <Route path="/" element={<App />} >
+          <Route path='login' element={<Login />} />
+          <Route path='dashboard' element={<Dashboard />} />
+        </Route>
+        {/* <Route path="add" element={<About />} /> */}
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
