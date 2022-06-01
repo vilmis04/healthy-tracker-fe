36cacffe9e5e0263ed7ba3d86d7a33dfdb1eb962
Vilmantas Sudaris <vilmantas.sudaris@gmail.com>
2022-06-01 18:21:52 +0300

    excess code removed

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -11,9 +11,6 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-// import AdbIcon from '@mui/icons-material/Adb';
-// import { Link as RouterLink } from 'react-router-dom';
-// import { Link as MuiLink } from '@mui/material';
 import './Navbar.css';
 import { IUser } from '../../App';
 import DrawerMenu from '../DrawerMenu/DrawerMenu';
@@ -78,43 +75,6 @@ const ResponsiveAppBar = (props: INavbarProps) => {
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
               <DrawerMenu />
-            {/* <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton> */}
-            {/* <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu> */}
-            <Drawer>
-
-            </Drawer>
           </Box>
           <Typography
             variant="h5"
